0039a95bcea3becbd878f57ab054656d28817084
Alexandre <aleleonel@gmail.com>
2026-06-23 20:03:42 -0300

    feat: expande dashboard com ranking e ultimas movimentacoes

--- relatorios/selectors.py
+++ relatorios/selectors.py
@@ -16,6 +16,10 @@ def get_dashboard_resumo(*, matriz):
         matriz=matriz
     )
 
+    cashback_base = LancamentoCashback.objects.filter(
+        matriz=matriz
+    )
+
     total_clientes = clientes_base.count()
 
     clientes_ativos = clientes_base.filter(
@@ -26,10 +30,6 @@ def get_dashboard_resumo(*, matriz):
         ativo=True,
         data_nascimento__month=mes_atual
     ).count()
-
-    cashback_base = LancamentoCashback.objects.filter(
-        matriz=matriz
-    )
 
     cashback_emitido = cashback_base.aggregate(
         total=Sum('valor_cashback')
@@ -47,6 +47,46 @@ def get_dashboard_resumo(*, matriz):
         total=Sum(F('valor_cashback') - F('valor_utilizado'))
     )['total'] or Decimal('0.00')
 
+    ultimos_clientes = clientes_base.filter(
+        ativo=True
+    ).only(
+        'id',
+        'nome',
+        'cpf',
+        'telefone',
+        'criado_em',
+    ).order_by(
+        '-criado_em'
+    )[:5]
+
+    ultimas_compras = cashback_base.select_related(
+        'cliente',
+        'loja'
+    ).only(
+        'id',
+        'cliente__nome',
+        'loja__nome',
+        'valor_compra',
+        'valor_cashback',
+        'data_compra',
+        'criado_em',
+    ).order_by(
+        '-criado_em'
+    )[:5]
+
+    top_clientes_cashback = cashback_base.values(
+        'cliente__id',
+        'cliente__nome',
+        'cliente__cpf',
+    ).annotate(
+        total_cashback=Sum('valor_cashback'),
+        total_utilizado=Sum('valor_utilizado'),
+        total_restante=Sum(F('valor_cashback') - F('valor_utilizado')),
+        total_compras=Count('id'),
+    ).order_by(
+        '-total_cashback'
+    )[:10]
+
     return {
         'total_clientes': total_clientes,
         'clientes_ativos': clientes_ativos,
@@ -54,4 +94,7 @@ def get_dashboard_resumo(*, matriz):
         'cashback_emitido': cashback_emitido,
         'cashback_utilizado': cashback_utilizado,
         'cashback_disponivel': cashback_disponivel,
+        'ultimos_clientes': ultimos_clientes,
+        'ultimas_compras': ultimas_compras,
+        'top_clientes_cashback': top_clientes_cashback,
     }
