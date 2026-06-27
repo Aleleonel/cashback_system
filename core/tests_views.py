ce8b411742bb734a23813f99f7324b3ba681f8bc
Alexandre <aleleonel@gmail.com>
2026-06-27 15:15:47 -0300

    fix: corrige select_related da fila de envios

--- core/tests_views.py
+++ core/tests_views.py
@@ -1,8 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from empresas.models import Loja, Matriz
+
 
 class ViewsProtegidasTest(TestCase):
+
+    def setUp(self):
+        self.User = get_user_model()
+
+        self.matriz = Matriz.objects.create(
+            nome='Matriz Teste'
+        )
+
+        self.loja = Loja.objects.create(
+            matriz=self.matriz,
+            nome='Loja Teste',
+            ativa=True
+        )
+
+        self.usuario = self.User.objects.create_user(
+            username='usuario_teste',
+            password='123456',
+            matriz=self.matriz
+        )
+
+        self.usuario.lojas.add(self.loja)
 
     def test_dashboard_exige_login(self):
         response = self.client.get(
@@ -51,3 +75,57 @@ class ViewsProtegidasTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertIn('/login/', response.url)
+
+    def test_dashboard_usuario_autenticado_com_contexto_valido(self):
+        self.client.force_login(self.usuario)
+
+        response = self.client.get(
+            reverse('relatorios:dashboard')
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_clientes_usuario_autenticado_com_contexto_valido(self):
+        self.client.force_login(self.usuario)
+
+        response = self.client.get(
+            reverse('clientes:lista_clientes')
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_nova_compra_usuario_autenticado_com_contexto_valido(self):
+        self.client.force_login(self.usuario)
+
+        response = self.client.get(
+            reverse('cashback:nova_compra')
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_aniversariantes_usuario_autenticado_com_contexto_valido(self):
+        self.client.force_login(self.usuario)
+
+        response = self.client.get(
+            reverse('campanhas:aniversariantes_mes')
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_historico_envios_usuario_autenticado_com_contexto_valido(self):
+        self.client.force_login(self.usuario)
+
+        response = self.client.get(
+            reverse('campanhas:historico_envios')
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_fila_envios_usuario_autenticado_com_contexto_valido(self):
+        self.client.force_login(self.usuario)
+
+        response = self.client.get(
+            reverse('campanhas:fila_envios')
+        )
+
+        self.assertEqual(response.status_code, 200)
