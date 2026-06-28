c7d7cee452961a582e68c1fc2d547c2b17d0a496
Alexandre <aleleonel@gmail.com>
2026-06-28 09:55:07 -0300

    feat: adiciona filtros visuais reutilizaveis

--- core/templatetags/ui_tags.py
+++ core/templatetags/ui_tags.py
@@ -1,3 +1,5 @@
+from decimal import Decimal, InvalidOperation
+
 from django import template
 
 register = template.Library()
@@ -41,3 +43,64 @@ def auditoria_icone(acao):
         acao,
         'bi-circle'
     )
+
+
+@register.filter
+def money(valor):
+
+    if valor is None:
+        return '0,00'
+
+    try:
+        valor_decimal = Decimal(valor)
+    except (InvalidOperation, TypeError, ValueError):
+        return '0,00'
+
+    return f'{valor_decimal:,.2f}'.replace(',', 'X').replace('.', ',').replace('X', '.')
+
+
+@register.filter
+def cpf(valor):
+
+    numeros = ''.join(
+        filter(str.isdigit, str(valor or ''))
+    )
+
+    if len(numeros) != 11:
+        return valor or ''
+
+    return f'{numeros[:3]}.{numeros[3:6]}.{numeros[6:9]}-{numeros[9:]}'
+
+
+@register.filter
+def telefone(valor):
+
+    numeros = ''.join(
+        filter(str.isdigit, str(valor or ''))
+    )
+
+    if len(numeros) == 11:
+        return f'({numeros[:2]}) {numeros[2:7]}-{numeros[7:]}'
+
+    if len(numeros) == 10:
+        return f'({numeros[:2]}) {numeros[2:6]}-{numeros[6:]}'
+
+    return valor or ''
+
+
+@register.simple_tag
+def boolean_badge(valor):
+
+    if valor:
+        return 'Sim'
+
+    return 'Não'
+
+
+@register.simple_tag
+def boolean_badge_class(valor):
+
+    if valor:
+        return 'bg-success'
+
+    return 'bg-secondary'
