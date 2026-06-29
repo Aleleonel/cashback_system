4102ae800592df1b5f01ec6a15f7e6f5d0e70776
Alexandre <aleleonel@gmail.com>
2026-06-29 09:47:10 -0300

    feat: adiciona servico de implantacao de empresa

---
+++ plataforma/services.py
@@ -0,0 +1,74 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
+
+from auditoria.models import RegistroAuditoria
+from auditoria.services import registrar_auditoria
+from core.choices import StatusOperacional
+from core.services import garantir_configuracao_sistema
+from empresas.models import Loja, Matriz
+
+
+def implantar_empresa(
+    *,
+    dados_matriz,
+    dados_loja,
+    dados_admin,
+    usuario_executor,
+    request=None
+):
+
+    User = get_user_model()
+
+    with transaction.atomic():
+
+        matriz = Matriz.objects.create(
+            nome=dados_matriz['nome'],
+            cnpj=dados_matriz.get('cnpj') or None,
+            status=StatusOperacional.ATIVA
+        )
+
+        loja = Loja.objects.create(
+            matriz=matriz,
+            nome=dados_loja['nome'],
+            cnpj=dados_loja.get('cnpj') or None,
+            telefone=dados_loja.get('telefone') or None,
+            status=StatusOperacional.ATIVA
+        )
+
+        usuario_admin = User.objects.create_user(
+            username=dados_admin['username'],
+            email=dados_admin.get('email') or '',
+            password=dados_admin['password'],
+            first_name=dados_admin.get('first_name') or '',
+            matriz=matriz,
+            perfil=User.PERFIL_MASTER,
+            ativo=True,
+        )
+
+        usuario_admin.lojas.add(loja)
+
+        garantir_configuracao_sistema(
+            matriz=matriz
+        )
+
+        registrar_auditoria(
+            usuario=usuario_executor,
+            matriz=matriz,
+            loja=loja,
+            acao=RegistroAuditoria.ACAO_CRIAR,
+            recurso='plataforma.implantacao_empresa',
+            recurso_id=matriz.id,
+            descricao=(
+                f'Empresa implantada. '
+                f'Matriz: {matriz.nome}. '
+                f'Loja principal: {loja.nome}. '
+                f'Administrador: {usuario_admin.username}.'
+            ),
+            request=request
+        )
+
+        return {
+            'matriz': matriz,
+            'loja': loja,
+            'usuario_admin': usuario_admin,
+        }
