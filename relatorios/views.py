cfdfc757d86fb6db90342a9c8a636e665b80d1ef
Alexandre <aleleonel@gmail.com>
2026-06-29 14:55:22 -0300

    security: aplica RBAC no dashboard operacional

--- relatorios/views.py
+++ relatorios/views.py
@@ -5,8 +5,12 @@ from core.services import get_contexto_operacional_usuario
 
 from .selectors import get_dashboard_resumo
 
+from accounts.decorators import require_permission
+from accounts.permissions import PERMISSAO_RELATORIOS_DASHBOARD
+
 
 @login_required
+@require_permission(PERMISSAO_RELATORIOS_DASHBOARD)
 def dashboard(request):
 
     contexto = get_contexto_operacional_usuario(
