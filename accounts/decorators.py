4da1720003d4c83a1190327434baba316d283ce5
Alexandre <aleleonel@gmail.com>
2026-06-29 15:27:28 -0300

    security: audita tentativas de acesso negado

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -2,6 +2,9 @@ from functools import wraps
 
 from django.core.exceptions import PermissionDenied
 
+from auditoria.models import RegistroAuditoria
+from auditoria.services import registrar_auditoria
+
 from .services import usuario_tem_permissao
 
 
@@ -16,6 +19,28 @@ def require_permission(permissao):
                 request.user,
                 permissao
             ):
+                usuario = (
+                    request.user
+                    if getattr(request.user, 'is_authenticated', False)
+                    else None
+                )
+
+                matriz = getattr(request.user, 'matriz', None) if usuario else None
+
+                registrar_auditoria(
+                    usuario=usuario,
+                    matriz=matriz,
+                    loja=None,
+                    acao=RegistroAuditoria.ACAO_ACESSAR,
+                    recurso='acesso.negado',
+                    descricao=(
+                        f'Acesso negado. '
+                        f'Permissão exigida: {permissao}. '
+                        f'View: {view_func.__name__}.'
+                    ),
+                    request=request
+                )
+
                 raise PermissionDenied(
                     'Você não tem permissão para acessar este recurso.'
                 )
