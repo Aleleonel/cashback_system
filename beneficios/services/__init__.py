8ef332bc206a0f79d681fd01fd8c391078b11bf6
Alexandre <aleleonel@gmail.com>
2026-07-04 21:33:21 -0300

    refactor: organiza services de beneficios por responsabilidade

---
+++ beneficios/services/__init__.py
@@ -0,0 +1,15 @@
+from .estrategia import (
+    calcular_desconto_voucher,
+    get_melhor_voucher,
+    selecionar_voucher_recomendado,
+)
+
+from .simulacao import (
+    calcular_cashback_sugerido,
+    simular_compra,
+    simular_beneficios,
+)
+
+from .validacao import (
+    validar_voucher_para_compra,
+)
